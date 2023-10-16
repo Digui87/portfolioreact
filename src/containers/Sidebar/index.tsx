c820d55d1a81b4c4c215c79edbacf3ebd3620260
Digui87 <d_salvador110@outlook.com>
2023-10-16 15:03:10 -0300

    estruturando o projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Digui87.png" />
+    <Titulo>Diagnas Salvador</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint distinctio
+      modi asperiores fuga ut at iusto? In perferendis sunt debitis vitae qui
+      dolorem assumenda ipsum, earum possimus neque. Perspiciatis, vitae.
+    </p>
+  </section>
+)
+
+export default Sobre
