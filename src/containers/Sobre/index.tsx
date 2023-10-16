17506e1cf9f324c999f6181b8452675bae49a2d3
Digui87 <d_salvador110@outlook.com>
2023-10-16 18:13:54 -0300

    criando a seção sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,19 @@
 import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
-    <Paragrafo tipo="secundario">
+    <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint distinctio
       modi asperiores fuga ut at iusto? In perferendis sunt debitis vitae qui
       dolorem assumenda ipsum, earum possimus neque. Perspiciatis, vitae.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=Digui87&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Digui87&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
